b5cb669826be054698ca584cf0dacab0891d535d
David Gullmarsvik <awt2.2012@gmail.com>
2012-09-04 10:36:21 +0200

    Uppgift 1 + 2

---
+++ Lektion8Tests/UserTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lektion8Tests
+{
+    [TestClass]
+    public class UserTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
